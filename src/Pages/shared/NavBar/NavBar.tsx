7bda2dc5e45bda9c1ef2eef64355a40e30dcc584
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-09-21 18:44:10 +0600

    navbar upgrade

--- src/Pages/shared/NavBar/NavBar.tsx
+++ src/Pages/shared/NavBar/NavBar.tsx
@@ -6,39 +6,39 @@ import DesktopNav from './DesktopNav';
 
 
 const NavBar = () => {
-    const [bg, setBg] = useState(false);
+  const [bg, setBg] = useState(false);
 
-    useEffect(() => {
-      window.addEventListener('scroll', () => {
-        return window.scrollY > 50 ? setBg(true) : setBg(false);
-      });
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      return window.scrollY > 50 ? setBg(true) : setBg(false);
     });
-    return (
-      <>
+  });
+  return (
+    <>
       <header
         className={`${bg ? 'bg-primary h-20' : 'h-24'
           } flex items-center fixed top-0 w-full px-5 text-white z-10 transition-all duration-300 `}
       >
-        <div className='container mx-auto h-full flex items-center justify-between'>
-          
+        <div className='container mx-auto h-full flex items-center justify-between w-10/12'>
+
           <div className='hidden lg:block'>
             <DesktopNav />
           </div>
           <div className='hidden lg:block'>
-          <a href='/' className=''>
-            <h3 className='w-48'> <img src={logo} alt="" /> </h3>
-          </a>
+            <a href='/' className=''>
+              <h3 className='w-48'> <img src={logo} alt="" /> </h3>
+            </a>
           </div>
           <div className='hidden lg:block'>
-            <NavEnd/>
+            <NavEnd />
           </div>
-          <div  className='lg:hidden'>
-            <NavMobile/>
+          <div className='lg:hidden'>
+            <NavMobile />
           </div>
         </div>
       </header>
     </>
-    );
+  );
 };
 
 export default NavBar;
--- src/Pages/shared/NavBar/NavMobile.tsx
+++ src/Pages/shared/NavBar/NavMobile.tsx
@@ -14,18 +14,18 @@ import { useQuery } from '@tanstack/react-query';
 const NavMobile = () => {
   const navigate = useNavigate();
   const [user] = useAuthState(auth);
-  const [admin]:any  =  useAdmin(user);
-  const [users]:any  =  useUser(user);
+  const [admin]: any = useAdmin(user);
+  const [users]: any = useUser(user);
   const email = user?.email;
   const { isLoading, error, data, refetch } = useQuery(['data'], () =>
-  fetch(`https://take-your-smile.onrender.com/user/${email}`, {
-    method: "GET",
-     headers: {
-      'content-type': 'application/json',
-      'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-     }
-}).then(res =>res.json())
-)
+    fetch(`https://take-your-smile.onrender.com/user/${email}`, {
+      method: "GET",
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    }).then(res => res.json())
+  )
 
   const logout = () => {
     signOut(auth);
@@ -33,73 +33,72 @@ const NavMobile = () => {
     localStorage.removeItem('accessToken');
   }
   const navigation = <>
-      {user ? <li className=' text-white  cursor-pointer uppercase'>
+    {user ? <li className=' text-white  cursor-pointer uppercase'>
 
-{/* Start */}
-<div className="dropdown dropdown-end">
-  <label id="0" className=" m-1"> <button className=" transition-all duration-300 uppercase"  >
+      {/* Start */}
+      <div className="dropdown dropdown-end">
+        <label id="0" className=" m-1"> <button className=" transition-all duration-300 uppercase"  >
 
-  <div className="avatar ">
+          <div className="avatar ">
             <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              {
+                data?.image ? <img src={data?.image} alt={data.name} className="w-12 h-12" /> : <img src="https://i.ibb.co/rwGPsQ9/profile.jpg" alt={data?.name} className="w-14 h-14" />}
+            </div>
+          </div></button> </label>
+        <ul id="0" className="dropdown-content menu p-2 shadow drop-bg rounded  w-52">
+          <div
+            className=" tracking-wider drop-bg uppercase px-1 py-2 w-64 mt-4  rounded max-w-screen origin-center  right-0 appear-done enter-done "
+          >
+
             {
-             data?.image ? <img src={data?.image} alt={data.name} className="w-12 h-12"  /> : <img src="https://i.ibb.co/rwGPsQ9/profile.jpg" alt={data?.name} className="w-14 h-14" />}
-              </div>
-              </div></button> </label>
-  <ul id="0" className="dropdown-content menu p-2 shadow drop-bg rounded  w-52">
+              users?.role === "user" &&
+              <>
+                <Link className="flex items-center px-3 py-3 cursor-pointer  text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/favourites"> My Favorites</Link>
+                <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/profiles"> Profile </Link>
+                <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/yourbookings"> Your Bookings</Link>
 
-    <div
-      className="drop-bg uppercase px-1 py-2 w-64 mt-4  rounded max-w-screen origin-center  right-0 appear-done enter-done"
-    >
+                <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/availablejob">Apply for Employee</Link>
+              </>
+            }
+            {
+              admin?.role === "Admin" && <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none  hover:bg-primary hover:text-white" to="/admin"> Admin DashBoard</Link>}
 
-{
-           users?.role === "user" && 
-           <>
-              <Link className="flex items-center px-3 py-3 cursor-pointer  text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/favourites"> My Favorites</Link>
-              <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/profiles"> Profile </Link>
-              <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/yourbookings"> Your Bookings</Link>
+            <button className="flex w-full items-center px-3 py-3 cursor-pointer  text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" onClick={logout} >LOGOUT</button>
+          </div>
+        </ul>
+      </div>
+      {/* end */}
 
-              <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" to="/availablejob">Apply for Employee</Link>
-            </> 
-        }
-                {
-                admin?.role === "Admin" && <Link className="flex items-center px-3 py-3 cursor-pointer text-secondary text-sm focus:outline-none  hover:bg-primary hover:text-white" to="/admin"> Admin DashBoard</Link>}
-
-                <button className="flex w-full items-center px-3 py-3 cursor-pointer  text-secondary text-sm focus:outline-none hover:bg-primary hover:text-white" onClick={logout} >LOGOUT</button>
-                  </div>
-                  </ul>
-                </div>
-                 {/* end */}
-
-               </li> : <li className=' text-secondary  cursor-pointer uppercase'><Link to='/login' 
-                      className='transition-all duration-300 '> Login</Link></li>}
+    </li> : <li className=' text-secondary  cursor-pointer uppercase'><Link to='/login'
+      className='transition-all duration-300 '> Login</Link></li>}
     <li className=' text-white  cursor-pointer'>
-      <Link to='/home'  className='transition-all duration-300 uppercase'> Home</Link></li>
-                             <li className=' text-white transition-all duration-300 cursor-pointer uppercase'>
-                              <div className="dropdown dropdown-hover">
-                                    <label id="0" className=" m-1">Portfolio</label>
-                                    <ul id="0" className="dropdown-content rounded menu p-2 shadow drop-bg     text-white w-52">
-                                      <li className=' text-secondary cursor-pointer uppercase'><Link to='/gallery' 
-                                          className='transition-all duration-300 hover:bg-primary hover:text-white'>Event Gallery</Link></li>
-                                      <li className=' text-secondary cursor-pointer uppercase'><Link to='/blogslike' 
-                                          className='transition-all duration-300 hover:bg-primary hover:text-white'>Blogs</Link></li>
-                                      <li className=' text-secondary cursor-pointer uppercase'><Link to='/donate' 
-                                          className='transition-all duration-300 hover:bg-primary hover:text-white'>Donation</Link></li>
-                                          <li className=' hover:bg-primary text-secondary hover:text-white  cursor-pointer uppercase'><Link to='/ourstory' 
-                                          className='transition-all duration-300'> Our Story</Link></li>
-                                    </ul>
-                                  </div>
-                              </li>
-                            
-                             <li className=' text-white  cursor-pointer uppercase'><Link to='/corporate' 
-                            className='transition-all duration-300'> Corporate</Link></li>
-                             <li className=' text-white  cursor-pointer uppercase'><Link to='/nonprofit' 
-                            className='transition-all duration-300'> Non-Profit</Link></li>
-                            <li className=' text-white  cursor-pointer'><Link to='/weddings'
+      <Link to='/home' className='transition-all duration-300 uppercase'> Home</Link></li>
+    <li className=' text-white transition-all duration-300 cursor-pointer uppercase'>
+      <div className="dropdown dropdown-hover">
+        <label id="0" className=" m-1">Portfolio</label>
+        <ul id="0" className="dropdown-content rounded menu p-2 shadow drop-bg     text-white w-52">
+          <li className=' text-secondary cursor-pointer uppercase'><Link to='/gallery'
+            className='transition-all duration-300 hover:bg-primary hover:text-white'>Event Gallery</Link></li>
+          <li className=' text-secondary cursor-pointer uppercase'><Link to='/blogslike'
+            className='transition-all duration-300 hover:bg-primary hover:text-white'>Blogs</Link></li>
+          <li className=' text-secondary cursor-pointer uppercase'><Link to='/donate'
+            className='transition-all duration-300 hover:bg-primary hover:text-white'>Donation</Link></li>
+          <li className=' hover:bg-primary text-secondary hover:text-white  cursor-pointer uppercase'><Link to='/ourstory'
+            className='transition-all duration-300'> Our Story</Link></li>
+        </ul>
+      </div>
+    </li>
+
+    <li className=' text-white  cursor-pointer uppercase'><Link to='/corporate'
+      className='transition-all duration-300'> Corporate</Link></li>
+    <li className=' text-white  cursor-pointer uppercase'><Link to='/nonprofit'
+      className='transition-all duration-300'> Non-Profit</Link></li>
+    <li className=' text-white  cursor-pointer'><Link to='/weddings'
       className='transition-all duration-300 uppercase'> Weddings</Link></li>
 
     <li className=' text-white  cursor-pointer uppercase'><Link to='/social'
       className='transition-all duration-300'> Social</Link></li>
- 
+
     <li className=' text-white cursor-pointer uppercase'><Link to='/articles' className='transition-all duration-300'>Article</Link></li>
 
 
