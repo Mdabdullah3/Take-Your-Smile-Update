dc1614cf644ad1981940ff8955b4dbf78050bdbb
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-09-21 17:56:50 +0600

    handle mode

--- src/Pages/LiveEvent/OurLiveEvent .tsx
+++ src/Pages/LiveEvent/OurLiveEvent .tsx
@@ -24,20 +24,20 @@ const OurLiveEvent = () => {
   const reverse = [...liveEvent].reverse()
   return (
     <div className="relative">
-      <Fade left duration={8500}>
+      <Fade left duration={6500}>
         <h1 className="md:text-[120px] text-4xl absolute -z-10 md:left-1/4 top-8 text-[#F7F7F7] text-center font-mono font-bold">latest Events</h1>
       </Fade>
-      <Fade left duration={8500}>
+      <Fade left duration={6500}>
         <h1 className="text-xl text-center font-mono text-secondary font-semibold">Upcoming Events</h1>
       </Fade>
-      <Fade right duration={8500}>
+      <Fade right duration={6500}>
         <h1 className="text-4xl my-2 text-center font-mono text-secondary font-semibold">Latest Awesome Events</h1>
       </Fade>
       <Swiper
         slidesPerView={1}
         centeredSlides={true}
         autoplay={{
-          delay: 10000,
+          delay: 12000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -50,18 +50,18 @@ const OurLiveEvent = () => {
           reverse.slice(0, 6).map((item: any) => (
             <SwiperSlide key={item._id}>
               <div>
-                <Fade left duration={10000}>
+                <Fade left duration={8000}>
                   <Time expiryTimestamp={time} />
                 </Fade>
                 <div className="w-6/12 mx-auto mb-16">
                   <div className="mx-auto w-10/12 mt-10">
-                    <Fade top duration={7500}>
+                    <Fade top duration={6000}>
                       <img className="w-80 md:w-[600px] md:h-[450px] relative mx-auto" src={item.decrImg2} alt="" />
                     </Fade>
-                    <Fade left duration={5500}>
+                    <Fade left duration={5000}>
                       <h1 className="md:absolute  md:top-32 left-1/3 bg-primary w-24  py-4 text-3xl px-4 font-bold text-white font-mono text-center rounded-lg"><FiBookmark className=" text-white ml-4" /> {item.date.slice(0, 6)}</h1>
                     </Fade>
-                    <Fade right duration={6500}>
+                    <Fade right duration={5500}>
                       <div className="font-mono absolute shadow-xl rounded-xl bg-[#00000084] md:top-2/4 top-1/4 md:left-2/4 left-1/2 px-4 py-4 md:w-72 w-auto">
                         <h1 className="text-xl text-white font-semibold">{item.service}</h1>
                         <h1 className="text-xl mt-2 font-semibold text-white flex items-center gap-2"><BiTime /> {item.time}</h1>
